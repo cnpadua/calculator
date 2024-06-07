e7da80182cae4530ff1445c9f26e945e3e3c639e
cnpadua <49352065+cnpadua@users.noreply.github.com>
2024-06-07 14:47:12 -0700

    Working on calculator functionality

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// GLOBAL
+let op1, op2, operator = null;
+
+// DEBUGGING
+function logOps(){
+    console.log(op1, op2, operator);
+}
+
+// CALCUALTOR OPERATORS
 function add(a,b) {
     return a+b;
 }
@@ -27,33 +36,73 @@ function clear(){
    display.textContent = "";
 }
 
+function getDisplayText(){
+    /*
+        Returns text in calculator display
+    */
+    let display = document.querySelector(".display");
+    return display.textContent;
+}
+
 function addToDisplay(text){
     /* 
         Adds text of button to display
     */
     console.log("addToDisplay()");
     let display = document.querySelector(".display");
-    let display_text = display.textContent;
-    display.textContent = display_text + text;
+    display.textContent = getDisplayText() + text;
     
 }
+
+function assignOperand(){
+    /* 
+
+    */
+}
+
 
 
 function main(){
     
-    let op1, op2, operator;
-
     // Clear button event listener
     let clear_button = document.querySelector(".clear");
     clear_button.addEventListener("click", clear);
 
-    // Number input event listener (updates display)
+    // Number input event listeners (updates display)
     let number_buttons = document.querySelectorAll(".number");
     [...number_buttons].map((btn_element) => 
         btn_element.addEventListener("click", (event)=>{
             addToDisplay(event.target.innerText);
     }));
+
+    // Operator event listeners
+    let opearator_buttons = document.querySelectorAll(".operator");
+    [...opearator_buttons].map((btn_element) => {
+        btn_element.addEventListener("click", (event) => {
+            clear();
+            assignOperand(getDisplayText());
+            logOps();
+        })
+    });
     
+    /*
+        Rubber Ducking
+        --------------
+
+        - AC resets
+        - When OPERATOR is FIRST pressed, save number in display
+        - Input second number
+        - Assigning operands
+            if op1 null
+                assign op1
+            else 
+                assign op2
+                calculate result
+                display result
+        
+        
+    
+    */
 }
 
 main();
